90bfc1af62dbdc08e0535727d529c64917b48df3
brettmt2 <btpeach.2@gmail.com>
2026-09-06 18:48:56 -0400

    handle google oauth callback response

--- src/app/main.py
+++ src/app/main.py
@@ -9,7 +9,16 @@ SCOPES = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
     "https://www.googleapis.com/auth/gmail.readonly",
+    "openid"
 ]
+
+flows = {}
+
+def build_flow():
+    flow = Flow.from_client_secrets_file('credentials.json', scopes=SCOPES)
+    flow.redirect_uri = 'http://localhost:8000/auth/callback'
+
+    return flow
 
 @app.get("/")
 def health():
@@ -20,8 +29,7 @@ def login():
     # trigger google OAuth flow
     # use my client credentials to authorize access into user google account
     # grab access token to call gmail API on behalf of user
-    flow = Flow.from_client_secrets_file('credentials.json', scopes=SCOPES)
-    flow.redirect_uri = 'http://localhost:8000/auth/callback'
+    flow = build_flow()
 
     # first step of the OAuth flow - generate the authorization URL
     # using configuration of my client
@@ -31,8 +39,19 @@ def login():
         prompt='consent'
     )
 
+    flows[state] = flow
+
     return RedirectResponse(url=authorization_url)
 
 @app.get("/auth/callback")
 def callback(request: Request):
-    pass
+    # use the same flow for state persistence
+    state = request.query_params.get("state")
+    flow = flows.pop(state)
+
+    # use the url to fetch access token and store it in the flow object
+    url = str(request.url)
+    flow.fetch_token(authorization_response=url)
+    credentials = flow.credentials
+
+    return {"status": "authenticated"}
