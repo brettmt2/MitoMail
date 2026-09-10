316b4a824237697962365b914df2a86e328ce150
brettmt2 <btpeach.2@gmail.com>
2026-09-10 17:05:04 -0400

    render info from api pull

--- src/web/api.js
+++ src/web/api.js
@@ -5,6 +5,16 @@ async function getProfileInfo() {
             throw new Error(`Response status: ${response.status}`);
         }
         const result = await response.json();
+                
+        // will always have a value from my backend
+        let photoURL = result.photo_url;
+        let name = result.name;
+
+        const profileImg = document.getElementById("profile-photo");
+        profileImg.src = photoURL;
+
+        const nameTitle = document.getElementById("profile-name");
+        nameTitle.textContent = name;
     }
     catch (error) {
         console.log(error.message);
